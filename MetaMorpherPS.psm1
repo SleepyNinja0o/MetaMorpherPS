6cf8e4a08c3be82d86b4d4a5a02df4ca85c7da0b
SleepyNinja <hillbilly.tiger@gmail.com>
2024-06-27 00:23:14 -0400

    Add files via upload

---
+++ MetaMorpherPS.psm1
@@ -0,0 +1,10 @@
+﻿# References:
+#    https://www.rickgouin.com/use-powershell-to-edit-mp3-tags
+#    https://github.com/mono/taglib-sharp
+
+[Reflection.Assembly]::LoadFrom("$PSScriptRoot\TagLibSharp.dll")
+
+function Get-FileMetadata {
+param($File)
+    return [TagLib.File]::Create((Get-ChildItem -Path $File).FullName)
+}
